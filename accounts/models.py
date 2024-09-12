6c0832a3ee70cb1f6f32a0df8d900e57bd3e3430
Kyuho09 <noaetoile1@gmail.com>
2024-09-12 17:12:23 +0900

    feat: accounts/models.py 수정
    필요한 필드 추가
    ImageField를 사용하기 때문에 settings.py에 MEDIA_URL, MEDIA_ROOT 설정
    이후 migrate까지 진행

--- accounts/models.py
+++ accounts/models.py
@@ -3,4 +3,11 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    pass
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    introductions = models.TextField(null=True, blank=True)
+    profile_image = models.ImageField(upload_to='profile_images/', null=True, blank=True)
+
+
+    def __str__(self):
+        return self.username
