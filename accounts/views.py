091e090ed0c1b2148f18858a48d92b9fd99dafaf
Kyuho09 <noaetoile1@gmail.com>
2024-09-12 19:43:46 +0900

    feat: 로그인 뷰 구현
    signin 경로와 view 구현
    djangorestframework-simplejwt install 후 freeze까지 진행
    settings.py에 REST_FRAMEWORK와 SIMPLE_JWT 설정

--- accounts/views.py
+++ accounts/views.py
@@ -4,8 +4,10 @@ from rest_framework.views import APIView
 from .models import User
 from .validators import validate_signup
 from .serializers import UserSerializer
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken
 
-# Create your views here.
+
 class SignupView(APIView):
     def post(self, request):
         is_valid, err_msg = validate_signup(request.data)
@@ -15,3 +17,18 @@ class SignupView(APIView):
         user = User.objects.create_user(**request.data)
         serializer = UserSerializer(user)
         return Response(serializer.data)
+
+
+class SigninView(APIView):
+    def post(self, request):
+        user = authenticate(**request.data)
+        if not user:
+            return Response(
+                {"error": "아이디 혹은 비밀번호가 올바르지 않습니다."}, status=400
+            )
+
+        res_data = UserSerializer(user).data
+        refresh = RefreshToken.for_user(user)
+        res_data["access_token"] = str(refresh.access_token)
+        res_data["refresh_token"] = str(refresh)
+        return Response(res_data)
--- django_times/settings.py
+++ django_times/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -131,3 +132,14 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    )
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+}
