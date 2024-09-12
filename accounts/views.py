3e066e7c696c90dffa5062ec20f1723531e57194
Kyuho09 <noaetoile1@gmail.com>
2024-09-12 19:21:42 +0900

    feat: validators, serializers 생성 및 회원가입 기능 구현
    validators.py 생성
    serializers.py 생성
    SignupView 구현

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from .models import User
+from .validators import validate_signup
+from .serializers import UserSerializer
 
 # Create your views here.
 class SignupView(APIView):
     def post(self, request):
-        pass
+        is_valid, err_msg = validate_signup(request.data)
+        if not is_valid:
+            return Response({"error": err_msg}, status=400)
+
+        user = User.objects.create_user(**request.data)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
