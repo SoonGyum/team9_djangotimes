088be0b5d1d5f4a1406b8ab8a849372439e31fdd
DESKTOP-NK05NPD\User <gyum9779@naver.com>
2024-09-13 14:58:56 +0900

    Category, Article의 model class 생성

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from accounts.models import User
 
-# Create your models here.
+
+class TimeStampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Category(models.Model):
+    CATEGORY_CHOICES = (
+        ("1", "정치"),
+        ("2", "경제"),
+        ("3", "사회"),
+        ("4", "생활/문화"),
+        ("5", "세계"),
+        ("6", "IT/과학"),
+    )
+    name = models.CharField(max_length=1, choices=CATEGORY_CHOICES)
+
+    def __str__(self):
+        return self.name
+
+
+class Article(TimeStampedModel):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    # url은 선택사항
+    url = models.URLField(max_length=200, blank=True, null=True)
+
+    def __str__(self):
+        return self.category, self.title, self.user
