d42f352b23aedea2c7111e473b44943967508413
DESKTOP-NK05NPD\User <gyum9779@naver.com>
2024-09-13 15:07:50 +0900

    article목록조회 view 생성

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from rest_framework.response import Response
+from .models import Article
+
 
 # Create your views here.
+class ArticleListView(ListAPIView):
+    queryset = Article.objects.all()  # 모든 Article 객체 가져오기
+    serializer_class = ArticleSerializer  # 직렬화에 사용할 serializer 지정
